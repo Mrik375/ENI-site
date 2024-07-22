1fbc685e8d7263babda86cfe6061b4e1c0395e90
Emeric <mrikpask@gmail.com>
2024-07-22 16:00:03 +0200

    First commit

---
+++ src/main/java/fr/eni/site/EniSiteApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EniSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/site/EniSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
