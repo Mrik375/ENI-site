781146c9ef4a0133352160e19844957d56d92147
Alain Precioso <alainprecioso@gmail.com>
2024-07-25 05:57:38 +0200

    Ajoute application-secrets.properties commme PropertySource

--- src/main/java/fr/eni/site/EniSiteApplication.java
+++ src/main/java/fr/eni/site/EniSiteApplication.java
@@ -2,8 +2,10 @@ package fr.eni.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-secrets.properties")
 public class EniSiteApplication {
 
 	public static void main(String[] args) {
