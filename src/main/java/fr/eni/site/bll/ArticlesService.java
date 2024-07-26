8998943647b21d10199c84da6142d265ec11919b
Alain Precioso <alainprecioso@gmail.com>
2024-07-26 07:36:13 +0200

    Ajoute la fonction findAll a ArticlesService
    Implémente aussi les fonction pour charger les qutres objets dans un ArticleAvendre

--- src/main/java/fr/eni/site/bll/ArticlesService.java
+++ src/main/java/fr/eni/site/bll/ArticlesService.java
@@ -1,4 +1,10 @@
 package fr.eni.site.bll;
 
+import fr.eni.site.bo.ArticleAVendre;
+
+import java.util.List;
+
 public interface ArticlesService {
+
+	List<ArticleAVendre> findAll();
 }
--- src/main/java/fr/eni/site/bll/impl/ArticlesServiceImpl.java
+++ src/main/java/fr/eni/site/bll/impl/ArticlesServiceImpl.java
@@ -5,6 +5,11 @@ import fr.eni.site.bll.services.AdresseService;
 import fr.eni.site.bll.services.ArticleAVendreService;
 import fr.eni.site.bll.services.CategorieService;
 import fr.eni.site.bll.services.UtilisateurService;
+import fr.eni.site.bo.ArticleAVendre;
+import fr.eni.site.bo.Categorie;
+import fr.eni.site.bo.Utilisateur;
+
+import java.util.List;
 
 public class ArticlesServiceImpl implements ArticlesService {
 
@@ -19,4 +24,30 @@ public class ArticlesServiceImpl implements ArticlesService {
 		this.utilisateurService = utilisateurService;
 		this.adresseService = adresseService;
 	}
+
+	@Override
+	public List<ArticleAVendre> findAll() {
+		List<ArticleAVendre> articles = articleAVendreService.getAllArticles();
+
+		articles.forEach(this::chargeCategorieDansArticle);
+		articles.forEach(this::chargeUtilisateurDansArticle);
+
+		return articles;
+	}
+
+	public void chargeCategorieDansArticle(ArticleAVendre article) {
+		article.setCategorie(categorieService.getCategorieById(article.getCategorie().getId()));
+	}
+
+	public void chargeUtilisateurDansArticle(ArticleAVendre article) {
+		Utilisateur vendeur = utilisateurService.getUtilisateur(article.getVendeur().getPseudo());
+		chargeAdresseDansUtilisateur(vendeur);
+		article.setVendeur(vendeur);
+	}
+
+	public void chargeAdresseDansUtilisateur(Utilisateur utilisateur) {
+		utilisateur.setAdresse(adresseService.getAdresseById(utilisateur.getAdresse().getId()));
+	}
+
+
 }
