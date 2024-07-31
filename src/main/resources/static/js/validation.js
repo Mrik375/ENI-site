c6bef2212329a45d4ecc4e7ad8bddf66399d69d7
Léo Ghirardi <leo.ghirardi@gmail.com>
2024-07-31 13:23:11 +0200

    Ajout du JS pour confirmation mot de passe
    - Creation de validation.js
    - Modification du frag-profile-form.html
    - Modification du view-creer-compte.html
    - Modification du site-error.css

---
+++ src/main/resources/static/js/validation.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var formulaire = document.getElementById("form-creer"); // Remplacez par l'ID de votre formulaire
+    formulaire.addEventListener("submit", function(e) {
+        var motDePasse = document.getElementById("motDePasse").value; // Remplacez par l'ID de votre champ de mot de passe
+        var confirmation = document.getElementById("confirmMDP").value; // Remplacez par l'ID de votre champ de confirmation
+        var messageErreur = document.getElementById("p-erreur"); // L'ID de votre paragraphe pour le message d'erreur
+
+		console.log("mot de passe : " + motDePasse + " confirmation : " + confirmation);
+
+         // Efface le message d'erreur précédent
+        messageErreur.textContent = "";
+
+        if(motDePasse !== confirmation) {
+            e.preventDefault(); // Empêche la soumission du formulaire
+            
+             // Ajoute le message d'erreur
+            messageErreur.textContent = "Les mots de passe ne correspondent pas.";
+            messageErreur.style.color = "#ff6a6a";
+        }
+    });
+});
